53f32d744ba9db35cfc7b156cc858523d8767969
Feng Lee <feng@emqx.io>
2018-07-18 15:59:27 +0800

    Use crypto:strong_rand_bytes/1 instead of crypto:rand_bytes/1

--- src/bcrypt_app.erl
+++ src/bcrypt_app.erl
@@ -1,6 +1,8 @@
 %% Copyright (c) 2011 Hunter Morris
 %% Distributed under the MIT license; see LICENSE for details.
+
 -module(bcrypt_app).
+
 -author('Hunter Morris <hunter.morris@smarkets.com>').
 
 -behaviour(application).
@@ -8,9 +10,7 @@
 -export([start/2, stop/1]).
 
 start(normal, _Args) ->
-    case bcrypt_sup:start_link() of
-        {ok, Pid}          -> {ok, Pid};
-        {error, _} = Error -> Error
-    end.
+    bcrypt_sup:start_link().
 
-stop(_State) -> ok.
+stop(_State) ->
+    ok.
--- src/bcrypt_sup.erl
+++ src/bcrypt_sup.erl
@@ -7,7 +7,8 @@
 
 -export([start_link/0, init/1]).
 
-start_link() -> supervisor:start_link({local, ?MODULE}, ?MODULE, []).
+start_link() ->
+    supervisor:start_link({local, ?MODULE}, ?MODULE, []).
 
 init([]) ->
     PortChildren
